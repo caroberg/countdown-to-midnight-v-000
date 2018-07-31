4cbf425e2f01806c7cef5aef1952000a524c6bdd
Caroline Berg <carolineeberg@gmail.com>
2018-07-31 20:36:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
-def countdown(number)
+
   number = 20
   while number > 0
-    puts "#{number} SECOND(S)!"
+    print "#{number} SECOND(S)!"
     number -= 1
   end
 
   puts "HAPPY NEW YEAR!"
-end
+
